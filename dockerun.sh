85be7046460d6f22fc56f77c2f8a998361556036
Michal Marhan <marhanmichal@gmail.com>
2022-01-29 16:23:59 +0100

    Update dockerun.sh

--- dockerun.sh
+++ dockerun.sh
@@ -1,6 +1,6 @@
 # docker build -f Dockerfile -t iptvudpscanner .
 #sudo ./pipework --direct-phys enp3s0 CONTAINERID 10.16.203.210/26@10.16.203.254
-docker run -it \
+docker run -itd \
   --network fyzicka \
   --ip 10.0.1.2 \
   -e PUID=1000 -e PGID=1000 \
